937fceb11714fc5715cb4e1ec77f4e438a01c23d
Krutik Naina <nenakrutik2004@gmail.com>
2025-12-18 20:39:55 +0530

    Add URL in index.py

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["https://event-management-xi-umber.vercel.app", "http://localhost:5173", "http://localhost:5000"],
+    origin: ["https://event-frontend-roan.vercel.app/", "http://localhost:5173", "http://localhost:5000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
